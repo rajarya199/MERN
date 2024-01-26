a1c32398249f4e098ded1710a499d1f82e497a68
rajarya199 <aryalrajan078@gmail.com>
2024-01-26 21:54:19 +0545

    inser product controller

---
+++ Express/controllers/productController.js
@@ -0,0 +1,19 @@
+const Product=require('../models/productModel')
+
+//to insert product
+
+exports.postProduct=async(req,res)=>{
+    let product=new Product({
+        product_name:req.body.product_name,
+        product_price:req.body.product_price,
+        countInStock:req.body.countInStock,
+        product_description:req.body.product_description,
+        product_image:req.body.product_image,
+        category:req.body.category
+    })
+    product=await product.save()
+    if(!product){
+        return res.status(400).json({error:'something went wrong'})
+    }
+    res.send(product)
+}
