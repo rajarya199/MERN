2adf881418489bb7fa1c429d200d2f5aef2ae35d
rajarya199 <aryalrajan078@gmail.com>
2024-01-26 21:54:35 +0545

    post product route

---
+++ Express/routes/productRoute.js
@@ -0,0 +1,6 @@
+const express=require('express')
+const { postProduct } = require('../controllers/productController')
+const router=express.Router()
+
+router.post('/postproduct',postProduct)
+module.exports=router
