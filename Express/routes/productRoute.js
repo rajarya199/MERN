9888cd5b69d5d5bede15a12468d3f22c6b1499c4
rajarya199 <aryalrajan078@gmail.com>
2024-01-30 00:07:01 +0545

    update product route

--- Express/routes/productRoute.js
+++ Express/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
-const { postProduct, productList, productDetails } = require('../controllers/productController')
+const { postProduct, productList, productDetails, updateProduct } = require('../controllers/productController')
 const router=express.Router()
 
 router.post('/postproduct',postProduct)
 router.get('/productlist',productList)
 router.get('/productdetails/:id',productDetails)
+router.put('/updateproduct/:id',updateProduct)
 module.exports=router
