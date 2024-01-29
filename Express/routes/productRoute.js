a55df06209d07d72aa06a3360b8ac003990f5458
rajarya199 <aryalrajan078@gmail.com>
2024-01-30 00:01:32 +0545

    product detail route

--- Express/routes/productRoute.js
+++ Express/routes/productRoute.js
@@ -1,7 +1,8 @@
 const express=require('express')
-const { postProduct, productList } = require('../controllers/productController')
+const { postProduct, productList, productDetails } = require('../controllers/productController')
 const router=express.Router()
 
 router.post('/postproduct',postProduct)
 router.get('/productlist',productList)
+router.get('/productdetails/:id',productDetails)
 module.exports=router
