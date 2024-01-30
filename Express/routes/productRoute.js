a2bc6c49a6c9c6d983bbe70b00ba9fd06717842a
rajarya199 <aryalrajan078@gmail.com>
2024-01-31 00:16:12 +0545

    delete product route

--- Express/routes/productRoute.js
+++ Express/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express=require('express')
-const { postProduct, productList, productDetails, updateProduct } = require('../controllers/productController')
+const { postProduct, productList, productDetails, updateProduct, deleteProduct } = require('../controllers/productController')
 const router=express.Router()
 
 router.post('/postproduct',postProduct)
 router.get('/productlist',productList)
 router.get('/productdetails/:id',productDetails)
 router.put('/updateproduct/:id',updateProduct)
+router.delete('/deleteproduct/:id',deleteProduct)
 module.exports=router
