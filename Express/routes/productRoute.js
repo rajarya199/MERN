9364e8583994ec10d8c6c22842f7c76d7941e984
rajarya199 <aryalrajan078@gmail.com>
2024-02-04 01:16:28 +0545

    post with validation

--- Express/routes/productRoute.js
+++ Express/routes/productRoute.js
@@ -1,11 +1,13 @@
 const express=require('express')
 const { postProduct, productList, productDetails, updateProduct, deleteProduct } = require('../controllers/productController')
 const router=express.Router()
-const upload=require('../middleware/fileUpload')
 
-router.post('/postproduct',upload.single('product_image'),postProduct)
+const upload=require('../middleware/fileUpload')
+const { productValidation, validation } = require('../validation/validator')
+
+router.post('/postproduct',upload.single('product_image'),productValidation, validation ,postProduct)
 router.get('/productlist',productList)
 router.get('/productdetails/:id',productDetails)
-router.put('/updateproduct/:id',upload.single('product_image'),updateProduct)
+router.put('/updateproduct/:id',upload.single('product_image'),productValidation, validation ,updateProduct)
 router.delete('/deleteproduct/:id',deleteProduct)
 module.exports=router
