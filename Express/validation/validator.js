7091a1ec5d4c75e3837fa7f985dd928660e49828
rajarya199 <aryalrajan078@gmail.com>
2024-02-04 01:16:07 +0545

    product validator

--- Express/validation/validator.js
+++ Express/validation/validator.js
@@ -5,6 +5,23 @@ exports.categoryValidation=[
     .isLength({min:3}).withMessage('category must be of at least 3 character')
 
 ]
+
+exports.productValidation=[
+    check('product_name','product name is requires').notEmpty()
+    .isLength({min:3}).withMessage('product name must be of at least 3 character'),
+
+    check('product_price','price is required').notEmpty()
+    .isNumeric().withMessage('price should be numerical value'),
+
+    check('countInStock','stock is required').notEmpty()
+    .isNumeric().withMessage('numerical value is required'),
+
+    check('product_description','description is required').notEmpty()
+    .isLength({min:20}).withMessage('description must be at least of 20 character'),
+    check('category','category is required').notEmpty()
+]
+
+
 //next middleware -if correct then push to next,coming fn 
 exports.validation=(req,res,next)=>{
     const errors=validationResult(req)
