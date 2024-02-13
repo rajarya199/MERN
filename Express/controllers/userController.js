400d3e56d3e710ce9c147034b939a41c1b9e4626
rajarya199 <aryalrajan078@gmail.com>
2024-02-13 19:38:02 +0545

    email confirmation

--- Express/controllers/userController.js
+++ Express/controllers/userController.js
@@ -1,50 +1,103 @@
-const User=require('../models/userModel')
-const Token=require('../models/tokenModel')
-const crypto =require('crypto')
-const sendEmail=require('../utils/setEmail')
+const User = require("../models/userModel");
+const Token = require("../models/tokenModel");
+const crypto = require("crypto");
+const sendEmail = require("../utils/setEmail");
 
 // for user register
-exports.postUser=async(req,res)=>{
-    let user=new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    })
+exports.postUser = async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-    //check if email is already registered
-    User.findOne({email:user.email})
-    .then(async data=>{
-        if(data){
-            return res.status(400).json({error:'email is already registered try with new one'})
-        }
-        else{
-            
-    user=await user.save()
-    if(!user){
-        return res.status(400).json({error:'unable to create an account'})
-    }
-    //create tokem and save it to the token model
-    let token =new Token({
-        token:crypto.randomBytes(16).toString('hex'),
-        userId:user._id
-    })
-    token=await token.save()
-    if(!token){
-        return res.status(400).json({error:"failed to create a token"})
-    }
-    //send email process
-    sendEmail({
-        from:'no-reply@ecommerce.com',
-        to:user.email,
-        subject:"Email verification link",
-        text:`hello,\n\n please verify your email by click in the below link:\n\n
-        http:\/\/${req.headers.host}\/api\/confirmation\/${token.token}`
+  //check if email is already registered
+  User.findOne({ email: user.email }).then(async (data) => {
+    if (data) {
+      return res
+        .status(400)
+        .json({ error: "email is already registered try with new one" });
+    } else {
+      user = await user.save();
+      if (!user) {
+        return res.status(400).json({ error: "unable to create an account" });
+      }
+      //create tokem and save it to the token model
+      let token = new Token({
+        token: crypto.randomBytes(16).toString("hex"),
+        userId: user._id,
+      });
+      token = await token.save();
+      if (!token) {
+        return res.status(400).json({ error: "failed to create a token" });
+      }
+      //send email process
+      sendEmail({
+        from: "no-reply@ecommerce.com",
+        to: user.email,
+        subject: "Email verification link",
+        text: `hello,\n\n please verify your email by click in the below link:\n\n
+        http:\/\/${req.headers.host}\/api\/confirmation\/${token.token}`,
         //http:localhost:8000/api/confirmation/3457777
+      });
+      res.send(user);
+    }
+  });
+};
 
+//post email confirmation
+exports.postEmailConfirmation = (req, res) => {
+  //at first find the valid/matching token
+  //token milesi matra confirmation send
+  Token.findOne({ token: req.params.token })
+    .then((token) => {
+      if (!token) {
+        return res
+          .status(400)
+          .json({ error: "invalid token or token may have expired" });
+      }
+      //if we found the valid then find the valid user for that token
+      User.findOne({ _id: token.userId }) //token ko user id sanga check
+        .then((user) => {
+          if (!user) {
+            return res
+              .status(400)
+              .json({
+                error: "we are unable to find the valid user for this token",
+              });
+          }
+          //check if user is already verified or not
+          if (user.isVerified) {
+            return res
+              .status(400)
+              .json({
+                error: "email is already verified ,please login to continue",
+              });
+          }
+          //save the verified user
+          user.isVerified = true; //isVerified from model
+          user
+            .save()
+            .then((user) => {
+              if (!user) {
+                return res 
+                  .status(400)
+                  .json({ error: "failed to verify the email" });
+              }
+              res.json({
+                message: "congrats,your email has been verified successfuly",
+              });
+            })
+            .catch((err) => {
+              return res.status(400).json({ error: err });
+            });
+        })
+
+        .catch((err) => {
+          return res.status(400).json({ error: err });
+        });
     })
-     res.send(user)
-
-     }
-    })
-
-}
+    .catch((err) => {
+      return res.status(400).json({ error: err });
+    });
+};
