2b7aa1658b0fe124fdcdbfedd38c8fd32823dca3
rajarya199 <aryalrajan078@gmail.com>
2024-02-13 23:18:35 +0545

    signin route

--- Express/routes/userRoute.js
+++ Express/routes/userRoute.js
@@ -1,7 +1,8 @@
 const express=require('express')
-const { postUser, postEmailConfirmation } = require('../controllers/userController')
+const { postUser, postEmailConfirmation, signIn } = require('../controllers/userController')
 const router=express.Router()
 
 router.post('/register',postUser)
 router.put('/confirmation/:token',postEmailConfirmation)
+router.post('/signin',signIn)
 module.exports=router
