0b110e939d422070d46b3b88447f9d446964242a
rajarya199 <aryalrajan078@gmail.com>
2024-02-13 19:38:22 +0545

    email confirmation route

--- Express/routes/userRoute.js
+++ Express/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express=require('express')
-const { postUser } = require('../controllers/userController')
+const { postUser, postEmailConfirmation } = require('../controllers/userController')
 const router=express.Router()
 
 router.post('/register',postUser)
-
+router.put('/confirmation/:token',postEmailConfirmation)
 module.exports=router
