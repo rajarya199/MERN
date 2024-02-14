54b138cac4c0e1f996c6bbeff092c13c008213ce
rajarya199 <aryalrajan078@gmail.com>
2024-02-14 23:58:25 +0545

    forget password route

--- Express/routes/userRoute.js
+++ Express/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express=require('express')
-const { postUser, postEmailConfirmation, signIn } = require('../controllers/userController')
+const { postUser, postEmailConfirmation, signIn, forgetpassword } = require('../controllers/userController')
 const router=express.Router()
 
 router.post('/register',postUser)
 router.put('/confirmation/:token',postEmailConfirmation)
 router.post('/signin',signIn)
+router.post('/forgetpassword',forgetpassword)
 module.exports=router
