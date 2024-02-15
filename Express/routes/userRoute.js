229be141f01ae38b8bba61d7ae51552886b8cfee
rajarya199 <aryalrajan078@gmail.com>
2024-02-15 23:09:01 +0545

    reset password route

--- Express/routes/userRoute.js
+++ Express/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express=require('express')
-const { postUser, postEmailConfirmation, signIn, forgetpassword } = require('../controllers/userController')
+const { postUser, postEmailConfirmation, signIn, forgetpassword, resetPassword } = require('../controllers/userController')
 const router=express.Router()
 
 router.post('/register',postUser)
 router.put('/confirmation/:token',postEmailConfirmation)
 router.post('/signin',signIn)
 router.post('/forgetpassword',forgetpassword)
+router.put('/resetpassword/:token',resetPassword)
 module.exports=router
